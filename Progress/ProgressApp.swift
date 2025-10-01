3e9ee21ac8de65bbb4c58c3ec9c9aef0383d4a90
Liya <koshelenkoliia6@gmail.com>
2025-10-01 11:09:02 -0700

    Initial Commit

---
+++ Progress/ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressApp.swift
+//  Progress
+//
+//  Created by Лия Кошеленко on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
