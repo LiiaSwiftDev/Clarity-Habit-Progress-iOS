97b718a4140004c6d0b277d850826ca11dd7ba44
Liya <koshelenkoliia6@gmail.com>
2025-12-07 20:14:13 -0800

    - add func currentWeek to goal card view - variable thisWeek check if specific goal have week card for this week or not - variable progressForThisWeek if yes count and show persentage, if no show 0

--- Progress/FirstScreen/GoalCardView.swift
+++ Progress/FirstScreen/GoalCardView.swift
@@ -12,20 +12,24 @@ struct GoalCardView: View {
     
     @Query private var allWeeks: [Week]
     
+    // хранит список всех целей
     var goalStorage: Goal
     @State private var marked: Int = 0
     
-    // это все фильтр чтобы получить progress bar последней недели. это как коробочка, в которую мы что-то кладём. В данном случае в неё мы положим последнюю неделю.
-    var lastWeek: Week {
-        // goalWeeks — имя этой новой коробочки. В ней будут только недели для нашей цели.
-        let goalWeek = allWeeks
-            .filter {
-                // тут мы фильтруем все недели и берем все недели к одной цели
-                $0.goal?.id == goalStorage.id
-            }
-        //недели сортируются от самой большой к самой маленькой, т.е. последняя неделя будет первой в списке.
-            .sorted { $0.startDate > $1.startDate }
-        return goalWeek.first ?? Week(goal: goalStorage, startDate: Date(), endDate: Date())
+    // проверяем либо эта неделя имеет дату пн и вс либо nil
+    var thisWeek: Week? {
+        // кладем нашу цель
+        currentWeek(goal: goalStorage)
+    }
+    
+    // посчитай процент для progress bar
+    var progressForThisWeek: Double {
+        // guard let — безопасно "распаковываем" опционал
+        // let week = thisWeek — если thisWeek не nil, то присваиваем значение константе week и продолжаем выполнять код ниже.
+        //else { return 0 } — если thisWeek nil, то выполняем тело else и выходим из функции сразу, в данном случае возвращаем 0.
+        guard let week = thisWeek else { return 0 }
+        // week - это именно эта неделя, markedDaysCount - сколько отмечено дней
+        return Double(week.markedDaysCount) / Double(goalStorage.timePerWeek)
     }
     
     var body: some View {
@@ -52,7 +56,7 @@ struct GoalCardView: View {
                         .foregroundStyle(Color.black)
                     Spacer()
                     
-                    ProgressBarView(myProgress: Double(lastWeek.markedDaysCount) / Double(goalStorage.timePerWeek), width: 80.0)
+                    ProgressBarView(myProgress: progressForThisWeek, width: 80.0)
                     
                 }
             }.padding(.horizontal, 20)
@@ -60,6 +64,36 @@ struct GoalCardView: View {
             
         }
         
+    }
+
+    
+    // Эта функция ищет ОДНУ текущую неделю для конкретной цели.
+    // -> Week? значит может такая неделя есть, а может и нет
+    func currentWeek(goal: Goal) -> Week? {
+        // создаем помощника по датам
+        let calendar = Calendar.current
+        // сегодняшняя дата
+        let today = Date()
+
+        // allWeeks.first - возьми первую неделю что подходит под условие в скобках
+        return allWeeks.first { week in
+            // Метод startOfDay(for:) делает время = 00:00, чтобы учитывать только день, игнорируя часы/минуты
+            // Было 12:20 стало 00:00
+            let start = calendar.startOfDay(for: week.startDate)
+            // Метод date(bySettingHour:minute:second:of:) устанавливает время = 23:59:59, чтобы воскресенье считалось полностью.
+            // конец недели = конец дня (23:59:59) воскресенья
+            let end = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: week.endDate)!
+
+            // мы типа проверяем все недели чтобы они подошли под это условие из 3 компонентов
+            // 1. цель должна быть той, что goal
+            // 2. сегодня не раньше начала недели
+            // 3. сегодня не позже конца недели
+            return week.goal?.id == goal.id &&
+            // Проверяем: сегодняшняя дата не раньше начала недели
+                   today >= start &&
+            // Проверяем: сегодняшняя дата не позже конца недели
+                   today <= end
+        }
     }
 }
 
