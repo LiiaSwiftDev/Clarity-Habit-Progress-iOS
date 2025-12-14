97032688d9b212f31d7f73e9304280d0a173d2b3
Liya <koshelenkoliia6@gmail.com>
2025-12-13 19:52:50 -0800

    - fix bug for ipad in detail view

--- Progress/SecondView/DetailView.swift
+++ Progress/SecondView/DetailView.swift
@@ -13,6 +13,9 @@ struct DetailView: View {
     
     @Environment(\.modelContext) private var context
     @Environment(\.dismiss) var dismiss
+    // Скажи мне, сколько места есть по ширине экрана прямо сейчас
+    @Environment(\.horizontalSizeClass) private var hSize
+    
     
     // "Я храню их в массиве allWeeks — это список всех недель." allWeeks — это полный массив всех недель, которые есть в базе, для всех целей. Например, если у тебя три цели, и у каждой есть по 2 недели, то allWeeks будет содержать все 6 объектов.
     @Query private var allWeeks: [Week]
@@ -60,7 +63,7 @@ struct DetailView: View {
                     Spacer()
                     
                 }
-
+                
                 HStack {
                     Text(progress.goal)
                         .font(.screanTitle)
@@ -73,43 +76,43 @@ struct DetailView: View {
                         .foregroundStyle(Color("TimesPerWeek"))
                     
                 }.padding(.horizontal)
-
-                    ScrollView(showsIndicators: false) {
-                        VStack(spacing: 16) {
-                            // "Для каждой недели создаём отдельную карточку."
-                            ForEach(goalWeeks, id: \.id) { week in
-                                // у кадлой карточки есть цель и неделя
-                                WeekCardView(progress: progress, week: week)
-                                    .animation(.easeInOut(duration: 0.5), value: goalWeeks)
-                                    .onTapGesture {
-                                        selectedWeek = week
-                                        showSheetComment = true
-                                    }
-                                    .onLongPressGesture {
-                                        weekToDelete = week
-                                        showAlert = true
-                                    }
-                                    .alert(isPresented: $showAlert) {
-                                        Alert(
-                                            title: Text("Delete Week"),
-                                            message: Text("Delete this week permanently? This action cannot be undone."),
-                                            // .destructive - make button red
-                                            primaryButton: .destructive(Text("Delete"), action: {
-                                                 
-                                                    if let week = weekToDelete {
-                                                        
-                                                                    context.delete(week)
-                                                                    try? context.save()
-                                                        
-                                                        TelemetryDeck.signal("Delete week")
-                                                                }
-                                            }),
-                                            secondaryButton: .cancel())
-                                    }
-                            }
-                           
-                        } .padding(.bottom, 160)
-                }    
+                
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 16) {
+                        // "Для каждой недели создаём отдельную карточку."
+                        ForEach(goalWeeks, id: \.id) { week in
+                            // у кадлой карточки есть цель и неделя
+                            WeekCardView(progress: progress, week: week)
+                                .animation(.easeInOut(duration: 0.5), value: goalWeeks)
+                                .onTapGesture {
+                                    selectedWeek = week
+                                    showSheetComment = true
+                                }
+                                .onLongPressGesture {
+                                    weekToDelete = week
+                                    showAlert = true
+                                }
+                                .alert(isPresented: $showAlert) {
+                                    Alert(
+                                        title: Text("Delete Week"),
+                                        message: Text("Delete this week permanently? This action cannot be undone."),
+                                        // .destructive - make button red
+                                        primaryButton: .destructive(Text("Delete"), action: {
+                                            
+                                            if let week = weekToDelete {
+                                                
+                                                context.delete(week)
+                                                try? context.save()
+                                                
+                                                TelemetryDeck.signal("Delete week")
+                                            }
+                                        }),
+                                        secondaryButton: .cancel())
+                                }
+                        }
+                        
+                    } .padding(.bottom, 160)
+                }
             }
             
             VStack {
@@ -134,8 +137,8 @@ struct DetailView: View {
                                 .shadow(color: Color("GrayOutside").opacity(0.9) ,radius: 0, x: -0.5, y: 0)
                                 .shadow(color: Color("GrayOutside").opacity(0.9) ,radius: 0, x: 0, y: 0.5)
                                 .shadow(color: Color("GrayOutside").opacity(0.9) ,radius: 0, x: 0, y: -0.5)
-                                
-                                
+                            
+                            
                             
                         } .frame(height: 60)
                             .padding(.bottom, 20)
@@ -145,12 +148,12 @@ struct DetailView: View {
                     }
                     
                     Spacer()
-
+                    
                 }
                 .background {
                     Color("Background")
                         .background(.thinMaterial)
-                        //.opacity(0.7)
+                    //.opacity(0.7)
                         .clipShape(.rect(topLeadingRadius: 15, topTrailingRadius: 15))
                         .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 4)
                         .ignoresSafeArea()
@@ -159,19 +162,21 @@ struct DetailView: View {
             
         } .navigationBarBackButtonHidden(true)
             .sheet(isPresented: $showSheet) {
-                // to do
-                AddWeekView(goal: progress)
-                    .presentationDetents([.fraction(0.7)])
-            }
-            .sheet(isPresented: $showSheetComment) {
-                if let selectedWeek {
-                    CommentView(goal: progress, week: selectedWeek)
-                        .presentationDetents([.fraction(0.85)])
+                if hSize == .compact {
+                    // iPhone
+                    AddWeekView(goal: progress)
+                        .presentationDetents([.fraction(0.7)])
+                } else {
+                    // iPad
+                    AddWeekView(goal: progress)
+                    // попробуй показать этот модальный экран в стиле popover вместо обычного sheet
+                        .presentationCompactAdaptation(.popover)
                 }
             }
-
+        
+        
             .onAppear {
-                TelemetryDeck.signal("Visited Detail Screen") 
+                TelemetryDeck.signal("Visited Detail Screen")
             }
         
     }
