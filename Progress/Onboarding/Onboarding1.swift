72ba0324a66f789c7866f7d49d303f6a4e599ba6
Liya <koshelenkoliia6@gmail.com>
2026-03-16 19:30:02 -0700

    - add geomerty reader to 3 onboarding view - finished testing bofore send to App Store

--- Progress/Onboarding/Onboarding1.swift
+++ Progress/Onboarding/Onboarding1.swift
@@ -12,78 +12,86 @@ struct Onboarding1: View {
     var actionButton: () -> Void
     
     var body: some View {
-        ZStack {
-            
-            Color(red: 149/255, green: 100/255, blue: 152/255)
-                .ignoresSafeArea()
-            
-            VStack {
- 
-                Spacer()
+        GeometryReader { geo in
+            ZStack {
+                // background
+                Color(red: 149/255, green: 100/255, blue: 152/255)
+                    .ignoresSafeArea()
                 
-                HStack {
-                    Image("cloud1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.bottom, 20)
-                        .padding(.trailing, 20)
+                VStack {
+     
+                    Spacer()
+                    // cloud images
+                    HStack {
+                        Image("cloud1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geo.size.width * 0.18)
+                        // 20 20
+                            .padding(.bottom, geo.size.height * 0.08)
+                            .padding(.trailing, geo.size.width * 0.08)
 
-                    Image("cloud1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.top, 60)
-                        .padding(.leading, 30)
+                        Image("cloud1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geo.size.width * 0.2)
+                        // 60 30
+                            .padding(.top, 60)
+                            .padding(.leading, 30)
+                        
+                        Image("cloud2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geo.size.width * 0.20)
+                        // 50 20
+                            .padding(.bottom, geo.size.height * 0.12)
+                            .padding(.leading, 20)
+                                                
+                    }
                     
-                    Image("cloud2")
+                    // Main image
+                    Image("girl")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 80)
+                        .frame(height: geo.size.height * 0.35)
                         .padding(.bottom, 50)
-                        .padding(.leading, 20)
                     
-                    
-                }
-                
-                Image("girl")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 300)
-                    .padding(.bottom, 50)
-                
-                Text("Welcome to Progress App!")
-                    .foregroundStyle(Color.white)
-                    .multilineTextAlignment(.center)
-                    .font(Font.system(size: 22, weight: .semibold))
-                    .padding(.bottom, 20)
-                    
-                
-                Text("No more waiting, glow up with every habit and become the person you want to be.")
-                    .foregroundStyle(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 40)
-
-            Button {
-                actionButton()
-            } label: {
-                ZStack {
-                    Image("Button")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 200, height: 42)
-                    
-                    Text("Start Now")
-                        .font(Font.system(size: 18, weight: .medium))
+                    // Text
+                    Text("Welcome to Progress App!")
                         .foregroundStyle(Color.white)
+                        .multilineTextAlignment(.center)
+                        .font(Font.system(size: 22, weight: .semibold))
+                        .padding(.bottom, geo.size.height * 0.02)
+                        
+                    
+                    Text("No more waiting, glow up with every habit and become the person you want to be.")
+                        .foregroundStyle(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, geo.size.height * 0.06)
+
+                    // Button
+                Button {
+                    actionButton()
+                } label: {
+                    ZStack {
+                        Image("Button")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geo.size.width * 0.6, height: 42)
+                        
+                        Text("Start Now")
+                            .font(Font.system(size: 18, weight: .medium))
+                            .foregroundStyle(Color.white)
+                    }
                 }
+                .padding(.bottom, geo.size.height * 0.1)
+                 
+                }
+                .padding(.horizontal, 40)
+                
             }
-            .padding(.bottom, 90)
-             
-            }
-            .padding(.horizontal, 40)
-            
         }
+        
 
     }
 
